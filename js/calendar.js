ab3a9ec42c257dd5b04aed4f6ee7af6d529030af
Yaroslav Volvach <60551329+YaroslavVolvach@users.noreply.github.com>
2023-02-02 10:43:40 -0500

    Update calendar.js
    Add card-wrapper

--- js/calendar.js
+++ js/calendar.js
@@ -6,16 +6,17 @@ let html;
 
 
 for(item of results){
-  html =  `<div class="card">
-              <img src="${item.feature_image}" alt="${item.name}"></img>
-              <div class="container">
-                  <h4><b>${item.name}</b></h4>
-                  <p>${item.description}</p>
-                  <div class="button-card"><a href="${item.url}"><h2>Detail</h2></a></div>
-              </div>
-              <div>
-                  <h3 >${String(item.date).slice(0, 10)}</h3>
-                  <h3>${item.location}</h3>
-            </div>`
+    html = `<div class="card-wraper">
+                <div class="card">
+                    <img src="${item.feature_image}" alt="${item.name}"></img>
+                     <div class="container">
+                         <h4><b>${item.name}</b></h4>
+                         <p>${item.description}</p>
+                        <div class="button-card"><a href="${item.url}"><h2>Detail</h2></a></div>
+                    </div>
+                    <h3 >${String(item.date).slice(0, 10)}</h3>
+                    <h3>${item.location}</h3>
+               </div>
+            </div>`   
     main.insertAdjacentHTML("afterbegin", html)
 }
